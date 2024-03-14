09b69aca6638ab84b4fed80ecab6aba6f956429a
MJ Valiid <ValiidProductions@gmail.com>
2024-03-14 18:16:02 -0400

    6 - change

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const _ = require('lodash')
 const item = [1, [2, [3, [4]]]]
 
 const newitem = _.flattenDeep(item);
-console.log(newitem)
-console.log('bob')
+console.log(newitem);
+console.log('bob');
